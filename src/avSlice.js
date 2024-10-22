503fbb193dcde1c48a1a0e2f9ca03d872fc55a5d
Luis Gudiel <lggj2310@gmail.com>
2024-10-22 01:06:39 -0600

    Functionality for Expense Planner

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,58 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      img: "https://pixabay.com/images/download/business-20031_640.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
   ],
 
 
   reducers: {
+    // Increments the quantity of a specific item in the state.
+    // It takes two parameters: state, action.
+    // If the item exists, it increments its quantity property by 1.
     incrementAvQuantity: (state, action) => {
-      
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
     },
+    // Decrements the quantity of a specific item in the state.
+    // It takes two parameters: state and action.
+    // If the item exists and its quantity is greater than 0, it decrements its quantity property by 1, 
+    // ensuring the quantity doesn't drop below 0 and indicates no more available items.
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if (item && item.quantity > 0) { 
+        item.quantity--;
+      }
     },
   },
 });
